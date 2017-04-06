ab52edcc70ba715a8fbbffca496039bceb4d5507
Henry Mollman <henry@runnable.com>
2017-04-06 11:28:07 -0700

    Add specific handler for route

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const Organization = require('models/organization')
 const log = require('util/logger').child({ module: 'runnable-web-panel/routes' })
 const InstanceService = require('util/InstanceService')
 const keypather = require('keypather')()
+const rollbar = require('rollbar')
 module.exports = function (app, addon) {
 
     runnableAPI.login()
@@ -77,6 +78,7 @@ module.exports = function (app, addon) {
         })
         .catch((err) => {
           log.trace(err)
+          rollbar.handleError(err, request)
           return res.render('web-panel', {
             instance: false,
             text: 'We couldn‘t find an environment for this issue.'
