05083425a5f864a46f68134d7c9132db8baec1b2
Henry Mollman <henry@runnable.com>
2017-04-06 12:04:45 -0700

    Testing errors

--- util/ErrorHandler.js
+++ util/ErrorHandler.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const rollbar = require('rollbar')
+const log = require('util/logger').child({ module: 'runnable-web-panel/util/ErrorHandler' })
 
 module.exports = class ErrorHandler {
 
@@ -9,6 +10,7 @@ module.exports = class ErrorHandler {
    *
    */
   static reportErrorToRollbar (err, req) {
+    console.log('\n\n\n', err.message, err, '\n\n\n')
     if (err.message === 'org not found') {
       return rollbar.handleErrorWithPayloadData(err, { level: 'info' }, req)
     }
